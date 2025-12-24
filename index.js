6468b4c7d63c7048b102a141fc41af7cacc39063
finnest <finnestkhalid999@gmail.com>
2025-12-24 17:12:04 +0300

    index.js

--- index.js
+++ index.js
@@ -114,3 +114,149 @@ function gameObject() {
         },
     };
 }
+
+
+
+
+// ---  Player Information ---
+
+function numPointsScored(playerName) {
+  const data = gameObject();
+  // Look for the player key in both home and away objects
+  const player = data.home.players[playerName] || data.away.players[playerName];
+  return player ? player.points : 0;
+}
+
+function shoeSize(playerName) {
+  const data = gameObject();
+  const player = data.home.players[playerName] || data.away.players[playerName];
+  return player ? player.shoe : 0;
+}
+
+// --- Retrieve Team Information ---
+
+function teamNames() {
+  const data = gameObject();
+  return [data.home.teamName, data.away.teamName];
+}
+
+function teamColors(teamName) {
+  const data = gameObject();
+  // Compare input teamName with the names in the data
+  if (data.home.teamName === teamName) return data.home.colors;
+  if (data.away.teamName === teamName) return data.away.colors;
+}
+
+// --- Retrieve Player Numbers and Statistics ---
+
+function playerNumbers(teamName) {
+  const data = gameObject();
+  // Select the correct team object
+  const team = data.home.teamName === teamName ? data.home : data.away;
+  const numbers = [];
+  // Use for...in to iterate over the keys (player names) in the players object
+  for (const name in team.players) {
+    numbers.push(team.players[name].number);
+  }
+  return numbers;
+}
+
+function playerStats(playerName) {
+  const data = gameObject();
+  // Return the entire nested object for that specific player
+  return data.home.players[playerName] || data.away.players[playerName];
+}
+
+// --- Advanced Challenge ---
+
+function bigShoeRebounds() {
+  const data = gameObject();
+  const allTeams = [data.home, data.away];
+  let largestShoe = 0;
+  let reboundsResult = 0;
+
+  allTeams.forEach(team => {
+    for (const name in team.players) {
+      const player = team.players[name];
+      if (player.shoe > largestShoe) {
+        largestShoe = player.shoe;
+        reboundsResult = player.rebounds;
+      }
+    }
+  });
+  return reboundsResult;
+}
+
+// --- Bonus Quiz ---
+
+function mostPointsScored() {
+  const data = gameObject();
+  let highscore = 0;
+  let leadingScorer = "";
+
+  [data.home, data.away].forEach(team => {
+    for (const name in team.players) {
+      if (team.players[name].points > highscore) {
+        highscore = team.players[name].points;
+        leadingScorer = name;
+      }
+    }
+  });
+  return leadingScorer;
+}
+
+function winningTeam() {
+  const data = gameObject();
+  // Helper to calculate totals for each team
+  const calculateTotal = (team) => {
+    let sum = 0;
+    for (const name in team.players) {
+      sum += team.players[name].points;
+    }
+    return sum;
+  };
+
+  const homeScore = calculateTotal(data.home);
+  const awayScore = calculateTotal(data.away);
+
+  return homeScore > awayScore ? data.home.teamName : data.away.teamName;
+}
+
+function playerWithLongestName() {
+  const data = gameObject();
+  let longestName = "";
+
+  [data.home, data.away].forEach(team => {
+    for (const name in team.players) {
+      if (name.length > longestName.length) {
+        longestName = name;
+      }
+    }
+  });
+  return longestName;
+}
+
+// --- Super Bonus Challenge ---
+
+function doesLongNameStealATon() {
+  const data = gameObject();
+  let longestName = "";
+  let mostSteals = -1;
+  let thiefName = "";
+
+  [data.home, data.away].forEach(team => {
+    for (const name in team.players) {
+      // Logic for longest name
+      if (name.length > longestName.length) longestName = name;
+      
+      // Logic for most steals
+      if (team.players[name].steals > mostSteals) {
+        mostSteals = team.players[name].steals;
+        thiefName = name;
+      }
+    }
+  });
+
+  // Returns true if the name with the most characters is the same name with the most steals
+  return longestName === thiefName;
+}
